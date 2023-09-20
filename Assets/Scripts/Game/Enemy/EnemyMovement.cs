07957662d80e8c3fdca20195c0c21571807d6390
Avishka Sathyanjana <baavishka@gmail.com>
2023-09-20 11:44:52 +0530

    add borders to restrict object movments and made bullets destroy when off screen

--- Assets/Scripts/Game/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Game/Enemy/EnemyMovement.cs
@@ -10,16 +10,20 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField]
     private float _rotationSpeed;
 
+    [SerializeField]
+    private float _screenBorder;
+
     private Rigidbody2D _rigidbody;
     private PlayerAwarenessController _playerAwarenessController;
     private Vector2 _targetDirection;
     private float _changeDirectionCooldown;  //time remaining until the next direction change
-
+    private Camera _camera;
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _playerAwarenessController = GetComponent<PlayerAwarenessController>();
         _targetDirection = transform.up;  //initial target direction is up
+        _camera = Camera.main;      //get the main camera
     }
 
     private void FixedUpdate()
@@ -33,6 +37,7 @@ public class EnemyMovement : MonoBehaviour
     {
         HandleRandomDirectionChange(); //enemy go random direction
         HandlePlayerTargeting();    //when seen a player
+        HandleEnemyOffScreen();
     }
 
     private void HandleRandomDirectionChange()
@@ -43,7 +48,7 @@ public class EnemyMovement : MonoBehaviour
         {
             float angleChange = Random.Range(-90f, 90f);  //random angle 
             Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward); //get a rotation
-            _targetDirection = rotation * _targetDirection;   
+            _targetDirection = rotation * _targetDirection;
 
             _changeDirectionCooldown = Random.Range(1f, 5f);  //random cooldown
         }
@@ -63,6 +68,23 @@ public class EnemyMovement : MonoBehaviour
         Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
 
         _rigidbody.SetRotation(rotation);
+    }
+
+    private void HandleEnemyOffScreen()
+    {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+
+        if ((screenPosition.x < _screenBorder && _targetDirection.x < 0) ||
+            (screenPosition.x > _camera.pixelWidth - _screenBorder && _targetDirection.x > 0))
+        {
+            _targetDirection = new Vector2(-_targetDirection.x, _targetDirection.y); //to call him back if he go off screen
+        }
+
+        if ((screenPosition.y < _screenBorder && _targetDirection.y < 0) ||
+            (screenPosition.y > _camera.pixelHeight - _screenBorder && _targetDirection.y > 0))
+        {
+            _targetDirection = new Vector2(_targetDirection.x, -_targetDirection.y); //to call him back if he go off screen
+        }
     }
 
     private void SetVelocity()
--- Assets/Scripts/Game/Player/Bullet.cs
+++ Assets/Scripts/Game/Player/Bullet.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private Camera _camera;
+    private void Awake()
+    {
+        _camera = Camera.main;    
+    }
+
+    private void Update()
+    {
+        DestroyWhenOffScreen();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<EnemyMovement>()) //collision happening to an enemy
         {
             Destroy(collision.gameObject); //destroy enemy
 
+            Destroy(gameObject); //destroy bullet
+        }
+    }
+
+     private void DestroyWhenOffScreen()
+    {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);  //get the screen position of the bullet and convert it to screen cordinate
+
+        if (screenPosition.x < 0 ||
+            screenPosition.x > _camera.pixelWidth ||
+            screenPosition.y < 0 ||
+            screenPosition.y > _camera.pixelHeight)
+        {
+            //then bullet is offscreen
             Destroy(gameObject); //destroy bullet
         }
     }
